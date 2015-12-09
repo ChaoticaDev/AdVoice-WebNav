92ee35981c75a3d9c1266c0c9930c6b0d1827025
UberSnip <developers@ubersnip.com>
2015-12-09 08:47:59 -0600

    Update demo.php
    Initial voice-driven app testing

--- demo.php
+++ demo.php
@@ -19,7 +19,10 @@
 			var result = event.results[event.results.length-1];
 			if(result.isFinal) {
             	$(".keyword_detection").html("<br/>"+result[0].transcript);
-				$.get("request.php", {req: result[0].transcript}, function(data){
+            	$(".reg_header2").val(result[0].transcript);
+				$(".reg_header2").blur();
+				next_step();
+				$.get("https://ubersnip.com/UberSnip_VoiceAPI/request.php", {req: result[0].transcript}, function(data){
 					$(".keyword_detection").html(data);
 					
 					if ( data.indexOf(");") > 0 ) {
@@ -56,12 +59,90 @@
 		
 	}
 </script>
+
+
+
+<script>
+	/*VOICE DRIVE REGISTRATION APP*/
+	
+	var reg_step = 0;
+	var reg_steps_headers = [[0,"Speak or type your first and last name"], [1,"Enter username"], [0,"Speak password phrase"]];
+	
+	function start_registration(){
+		init=true;
+		$(".reg_header2").change( function(){
+			next_step();
+		});
+		next_step();	
+	}
+	
+	var init = false;
+	
+	function next_step(){
+		if ( !init ) { return ; }
+		switch ( reg_step ){
+			case 0:
+				//$(".reg_flname").val($(".reg_header2").val());
+				break;
+				
+			case 1:
+				
+				$(".reg_flname").val($(".reg_header2").val());
+				break;
+					
+			case 2:
+			
+				$(".reg_username").val($(".reg_header2").val());
+				break;	
+				
+			case 3:
+			
+				$(".reg_password").val($(".reg_header2").val());
+				break;	
+		}
+		if ( reg_step < reg_steps_headers.length ){
+			$(".reg_header").html(reg_steps_headers[reg_step][1]);
+		}
+		reg_step++;
+		
+		if ( reg_step > reg_steps_headers.length ){
+			init = false;	
+		}
+	}
+</script>
+
+<meta name="twitter:card" content="photo" />
+<meta name="twitter:site" content="@MontrayDavis" />
+<meta name="twitter:creator" content="@UberSnip" />
+<meta name="twitter:owner" content="@MontrayDavis" />
+<meta property="twitter:description" content="Control your website with voice commands! || #OpenSource" />
+<meta property="twitter:title" content="ÜberSnip - AdVoice WebNavigation." />
+<meta property="twitter:image" content="https://image.freepik.com/free-vector/scream-mouth-background_23-2147492625.jpg" />
+<meta property="twitter:url" content="https://ubersnip.com/advoice" />
+<meta property="og:url" content="https://ubersnip.com/advoice" />
+<meta property="og:title" content="ÜberSnip - AdVoice WebNavigation." />
+<meta property="og:description" content="Control your web browser with voice commands! OpenSource project." />
+
 </head>
 
 <body>
 <!--<button onclick="listen()">Start recording! (Requires microphone and Desktop Chrome web browser)</button>-->
 <div class="keyword_detection">
-	
+	Say a command!<br />
+    <strong>Command List</strong><br />
+    "Search for %s" : 'Search for cats and dogs'
 </div>
+
+<br /><br />
+<strong>EXAMPLE VOICE DRIVEN REGISTRATION APP</strong>
+<form>
+    <input class="reg_header2" type="hidden" />
+	<strong class="reg_header">Say "Register" to begin</strong><br />
+	<input type="text" class="reg_flname" /><br />
+	<input type="text" class="reg_username" /><br />
+    <input type="password" class="reg_password" /><br />
+    <button class="reg_nextbtn" onclick="next_step()">Next</button>
+</form><br /><br />
+<strong><a href="https://github.com/UberSnip/AdVoice-WebNav/blob/master/README.md">VIEW ON GITHUB</a></strong>
 </body>
 </html>
