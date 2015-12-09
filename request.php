5ccd6ffeb1656174072c46e1e9736614ead2b15c
UberSnip <developers@ubersnip.com>
2015-12-09 14:24:06 -0600

    Update request.php
    UPDATE: Added support for requiring specific keywords in a phrase. "%a {Values|Separated}"

--- request.php
+++ request.php
@@ -22,6 +22,7 @@
 			$words = implode ( $this->base_string ) ;
 			
 			$cmd_fnd = false;
+			$optionals=false;
 			foreach ( $CMDS as $CMD ){
 				$cmd_req_words = explode ( " ", $CMD->command_format );	
 				
@@ -29,13 +30,40 @@
 				foreach ( $cmd_req_words as $wd ){
 					if ( $wd == "%s" ){
 						break;	
+					}else if ($wd == "%a"){
+						$optionals = true;
+						break;
 					}
 					
 					$req .= $wd . " ";
 				}
+				$isOptional = false;
 				//echo $this->base_string;
+				if ( $optionals ){
+					$options = $cmd_req_words[sizeof($cmd_req_words)-1];
+					//echo $options;
+					
+					$options = str_replace("{", "", $options);
+					$options = str_replace("}", "", $options);
+					
+					$toptions = explode("|", $options);
+					
+					//echo substr($CMD->command_format, strpos($CMD->command_format, "%a")+3);
+					
+					$isOptional = false;
+					foreach ( $toptions as $topt ){
+						//echo substr($CMD->command_format, 0, strpos($CMD->command_format, "%a")) . $topt;	
+						
+						if ( $this->base_string == substr($CMD->command_format, 0, strpos($CMD->command_format, "%a")) . $topt ){
+							$isOptional=true;
+							//echo $CMD->command_format . strpos($CMD->command_format, "%a");
+							//die();
+						}
+					}
+				}
 				
-				if(strpos($this->base_string,$req) !== FALSE || $this->base_string == $CMD->command_format){
+				if((strpos($this->base_string,$req) !== FALSE && $isOptional != FALSE ) || $this->base_string == $CMD->command_format ){
+					
 					$search_sub_len = strlen(substr($this->base_string, strpos($this->base_string,$req), strlen($req)));
 					//echo strpos($this->base_string,$req) ."-" . strlen($req) . "::" . substr($this->base_string, strpos($this->base_string,$req)+$search_sub_len, strlen($this->base_string)) . "** ";
 					//echo "Command found";	
