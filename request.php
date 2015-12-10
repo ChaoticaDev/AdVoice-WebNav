77455392a0f1408c8100ec0e1678037ed12904ef
UberSnip <developers@ubersnip.com>
2015-12-10 11:13:36 -0600

    Update request.php
    Minor fixes

--- request.php
+++ request.php
@@ -25,22 +25,28 @@
 			
 			$cmd_fnd = false;
 			$optionals=false;
+			$disqualifies = false;
 			$wildcard = false;
 			$indexed = false;
+			$isDisqualified = false;
 			foreach ( $CMDS as $CMD ){
 				$cmd_req_words = explode ( " ", $CMD->command_format );	
 				
 				$req = "";
+				$disqualifies = false;
 				foreach ( $cmd_req_words as $wd ){
-					if ( $wd == "%s" ){
+					if ( strcmp($wd, "%s") === 0 ){
 						break;	
-					}else if ($wd == "%a"){
+					}else if (strcmp($wd, "%s^**") === 0){ //DISQUALIFIER
+						$disqualifies = true;
+						break;
+					}else if (strcmp($wd, "%a") === 0){ //FILTER
 						$optionals = true;
 						break;
-					}else if ($wd == "%s**"){ //WILDCARD
+					}else if (strcmp($wd, "%s**") === 0){ //WILDCARD
 						$wildcard = true;
 						break;
-					}else if ($wd == "%i**"){ //WILDCARD
+					}else if (strcmp($wd, "%i**") === 0){ //INDEX
 						$indexed = true;
 						break;
 					}
@@ -71,6 +77,29 @@
 					}
 				}
 				
+				if ( $disqualifies ){
+					$options = $cmd_req_words[sizeof($cmd_req_words)-1];
+					//echo $options;
+					
+					$options = str_replace("{", "", $options);
+					$options = str_replace("}", "", $options);
+					
+					$toptions = explode("|", $options);
+					
+					//echo substr($CMD->command_format, strpos($CMD->command_format, "%a")+3);
+					
+					$isOptional = false;
+					$isIndexed = false;	
+					
+					foreach ( $toptions as $topt ){	
+						
+						if ( $this->base_string == substr($CMD->command_format, 0, strpos($CMD->command_format, "%s^**")) . $topt ){
+							$isDisqualified=true;
+							$cmd_fnd=false;
+						}
+					}
+				}
+				
 				if ( $indexed ){
 					$options = $cmd_req_words[sizeof($cmd_req_words)-1];
 					//echo $options;
@@ -86,28 +115,28 @@
 					$q = mysqli_query ( $this->db, "SELECT * FROM ".$toptions[0]." WHERE `".$toptions[1]."` = '".$this->base_string."'" );
 					
 					if ( mysqli_num_rows ( $q ) > 0 ){
-						$isIndexed = true;	
+						$isIndexed = true;
 					}
 					
 				}
 				
-				if( (strpos($this->base_string,$req) !== FALSE && $isOptional == FALSE ) || $this->base_string == $CMD->command_format || $isOptional == true || $wildcard || $isIndexed){
+				if( (strpos($this->base_string,$req) !== FALSE && $isOptional == FALSE ) || $this->base_string == $CMD->command_format || $isOptional == true || $wildcard || $isIndexed ){
 					
 					//GET SUB_STRING LENGTH OF BASE_STRING UP UNTIL REQ_STRING FOUND
 					$search_sub_len = strlen(substr($this->base_string, strpos($this->base_string,$req), strlen($req)));
 					
 					$CMD->command_response = str_replace("{uber:request_param}", '"'.substr($this->base_string, strpos($this->base_string,$req)+$search_sub_len, strlen($this->base_string)).'"', $CMD->command_response);
 					
-					$cmd_fnd=true;
+						$cmd_fnd=true;
 					
 					//FINAL RESPONSE
-					echo $CMD->command_response;
 					break;
+					
 				} else{
 						
 				}
 			}
-			if ( !$cmd_fnd) { echo "NO COMMAND FOUND: " . $this->base_string; }
+			if ( !$cmd_fnd || $isDisqualified ) { echo '{"command":false}'; }else if ( !$isDisqualified && $cmd_fnd){ echo $CMD->command_response; }
 		}
 		
 	}
