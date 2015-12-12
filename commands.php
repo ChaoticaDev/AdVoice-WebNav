897f753094f403384922ece82d71682cc83847c8
UberSnip <developers@ubersnip.com>
2015-12-12 07:56:55 -0600

    Update commands.php
    Change in API: Must use "%a {word_here}  or  %s**" for single word commands.

--- commands.php
+++ commands.php
@@ -2,13 +2,13 @@
 	$CMD;
 	$TCMD = new COMMAND_CENTRAL();
 	$TCMD2 = new COMMAND_CENTRAL();
-	$TCMD->command_format = "search for %s";
+	$TCMD->command_format = "search for %s and cats";
 	$TCMD->command_response = "voice_search({uber:request_param});";
 	
 	$CMD[0] = $TCMD;
 	
 	$TCMD2 = new COMMAND_CENTRAL();
-	$TCMD2->command_format = "register";
+	$TCMD2->command_format = "%a {register}";
 	$TCMD2->command_response = "start_registration();";
 	
 	$CMD[1] = $TCMD2;
@@ -18,5 +18,11 @@
 	$TCMD3->command_response = "{uber:request_param}is my favorite artist! No problem! :D";
 	
 	$CMD[2] = $TCMD3;
+	
+	$TCMD3 = new COMMAND_CENTRAL();
+	$TCMD3->command_format = "search for %s^** {pie}";
+	$TCMD3->command_response = "{uber:request_param}is my favorite artist! No problem! :D";
+	
+	$CMD[3] = $TCMD3;
 	$request = new REQUEST_BUILDER();
 ?>
